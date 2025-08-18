507d59efcbb89ee722531fa02af07ccfea5b8e6d
mohand <mohanadhamadaf@gmail.com>
2025-08-18 15:38:16 +0300

    Lab 1 & 2 : Making structure and data base lab 1 but I would't commit it then make lab 2 ,Addition to initlaization of category APP. (Lab 3)

---
+++ products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Product(models.Model):
+    code_num = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    instock_items = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
