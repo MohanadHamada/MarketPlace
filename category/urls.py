c06a4624053c8e5d43f0665214df8b57ae564f98
mohand <mohanadhamadaf@gmail.com>
2025-08-18 17:45:04 +0300

    LAB 3: Append the pattern of url to call the creation category function (Login verfied) by applying Generic Views

--- category/urls.py
+++ category/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.decorators import login_required
+
+app_name = 'category'
 
 urlpatterns = [
     path('<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
     path('', CategoryListView.as_view(), name='category_list'),
+    path('create/', login_required(CategoryCreateView.as_view()), name='category_create'),
 ]
