48bc208d3fc8a4372f257f9a4f403ffa204b72be
mohand <mohanadhamadaf@gmail.com>
2025-08-18 17:13:24 +0300

    LAB 3: Append at category/urls.html the pattern of urls for List View Category and test each the function caller and html file of list category

--- category/urls.py
+++ category/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('', CategoryListView.as_view(), name='category_list'),
 ]
