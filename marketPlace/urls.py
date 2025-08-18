67157f95ff07541eedbd84037df49edbffe07097
mohand <mohanadhamadaf@gmail.com>
2025-08-18 18:42:49 +0300

    LAB 3: Edit marketPlace/urls.py to append the pattern to use Django Auth.

--- marketPlace/urls.py
+++ marketPlace/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 def home(request):
     return HttpResponse("<h1>Welcome to MarketPlace Home Page</h1>")
@@ -30,6 +33,15 @@ urlpatterns = [
     path('contactus/', include('contactus.urls')),
     path('aboutus/', include('aboutus.urls')),
     path('categories/', include('category.urls')),
+
+    
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/register/', CreateView.as_view(
+        form_class=UserCreationForm,
+        template_name='registration/register.html',
+        success_url='/accounts/login/'
+    ), name='register'),
 ]
 
 
